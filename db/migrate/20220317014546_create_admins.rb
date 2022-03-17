fac31402b911a1ae6b7b12c9f3b3d94ab01ed89d
taku472 <taku.3303@icloud.com>
2022-03-17 03:31:39 +0000

    model addition

--- db/migrate/20220317014546_create_admins.rb
+++ db/migrate/20220317014546_create_admins.rb
@@ -1,7 +1,10 @@
 class CreateAdmins < ActiveRecord::Migration[6.1]
   def change
     create_table :admins do |t|
-      
+      t.string :email
+      t.string :encrypted_password
+      t.datetime :created_at, null: false, default: "now"
+      t.datetime :updated_at, null: false, default: "now"
       t.timestamps
     end
   end
