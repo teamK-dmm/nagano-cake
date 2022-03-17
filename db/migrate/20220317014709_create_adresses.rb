b7ddca4be736ab338d225af35b91afe0565fb59a
EC2 Default User <ec2-user@ip-172-31-41-214.ec2.internal>
2022-03-17 03:28:35 +0000

    カラム追加

--- db/migrate/20220317014709_create_adresses.rb
+++ db/migrate/20220317014709_create_adresses.rb
@@ -1,6 +1,11 @@
 class CreateAdresses < ActiveRecord::Migration[6.1]
   def change
     create_table :adresses do |t|
+      t.string :receiver_name
+      t.string :address
+      t.string :postal_code
+      t.datetime :created_at, null: false, default: "now"
+      t.datetime :updated_at, null: false, default: "now"
 
       t.timestamps
     end
--- db/migrate/20220317014732_create_oder_items.rb
+++ db/migrate/20220317014732_create_oder_items.rb
@@ -1,6 +1,11 @@
 class CreateOderItems < ActiveRecord::Migration[6.1]
   def change
     create_table :oder_items do |t|
+      t.string :making_status
+      t.string :count
+      t.string :price
+      t.datetime :created_at, null: false, default: "now"
+      t.datetime :updated_at, null: false, default: "now"
 
       t.timestamps
     end
