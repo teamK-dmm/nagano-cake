9a0d02db01a706e7d7ab4757f73dd59d291e5a52
EC2 Default User <ec2-user@ip-172-31-17-32.ec2.internal>
2022-03-20 06:33:34 +0000

    itemsコントローラー記述

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,11 +10,30 @@ class Admin::ItemsController < ApplicationController
 
   def create
    @item = Item.new
+    if @item.save
+      redirect_to admin_item_path(@item), notice: "You have created item successfully."
+    else
+      @items = Item.all
+      render 'index'
+    end
   end
 
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to edit_admin_path(@item), notice: "You have updated book successfully."
+    else
+      render :edit
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   namespace :admin do
-    resources :items, only: [:index, :show, :edit, :update]
+    resources :items, only: [:index, :show, :edit, :update, :create]
   end
 
   namespace :admin do
