466c2dc90980bf2ba039675494f0a9fa79d65790
taku472 <taku.3303@icloud.com>
2022-03-21 07:05:33 +0000

    migarte追記

--- db/migrate/20220319091607_create_addresses.rb
+++ db/migrate/20220319091607_create_addresses.rb
@@ -1,6 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :addresses do |t|
+      t.string :customer_id
       t.string :receiver_name
       t.string :address
       t.string :postal_code
