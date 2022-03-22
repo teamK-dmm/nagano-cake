9eba997ae2ae08df6db1a6a5e4efcbf6b05bfb13
EC2 Default User <ec2-user@ip-172-31-41-214.ec2.internal>
2022-03-22 11:28:33 +0000

    顧客側注文関係コントローラー90%

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
     root to: "public/homes#top"
     get "/about" => "public/homes#about", as: "about"
 
-    get "customers/edit"=>"public/customers#edit", as: "customer_edit"
+    get "customers/edit"=>"public/customers#edit"
 
   devise_for :customers,skip:[:passwords], controllers: {registrations: "public/registrations",sessions: 'public/sessions'}
   devise_for :admin,skip:[:registrations, :passwords], controllers: {sessions: "admin/sessions"}
   namespace :public do
     resources :items, only:[:index, :show]
-    resources :orders, only:[:new, :index, :show, :thanks, :create, :log]
+    resources :orders, only:[:new, :index, :show, :create]
+    post "orders/log"=>"orders#log"
+    get "orders/thanks"=>"oeders#thanks"
   end
 
   resources :genres
