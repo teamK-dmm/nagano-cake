0041a73f7c79c4887784df411b83aa94fadb191c
EC2 Default User <ec2-user@ip-172-31-17-194.ec2.internal>
2022-03-23 12:15:05 +0000

    管理者側　注文履歴詳細　未完

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,6 +2,10 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    @total = 0
+    @order_items.each do |order_item|
+    @total = @total+order_item.count*order_item.item.price
+    end
   end
-
 end
