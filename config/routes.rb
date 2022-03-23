02e3a92ae439c302e64bd86c519c4fe92142571f
EC2 Default User <ec2-user@ip-172-31-17-194.ec2.internal>
2022-03-23 05:20:50 +0000

    ジャンル移動できるようになった

--- config/routes.rb
+++ config/routes.rb
@@ -3,36 +3,32 @@ Rails.application.routes.draw do
     # Top
     root to: "public/homes#top"
     get "/about" => "public/homes#about", as: "about"
-
     #customer/edit
     get "customers/edit"=>"public/customers#edit", as: "customer_edit"
-
-  devise_for :customers,skip:[:passwords], controllers: {registrations: "public/registrations",sessions: 'public/sessions'}
-  devise_for :admin,skip:[:registrations, :passwords], controllers: {sessions: "admin/sessions"}
-  namespace :public do
-    resources :items, only:[:index, :show]
-    resources :orders, only:[:new, :index, :show, :thanks, :create, :log]
-  end
-
-  resources :genres
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :admin do 
-    root to: "homes#top"
-    resources :orders_items
-    resources :orders
-    resources :items, only: [:index, :show, :edit, :update, :create, :new]
-    resources :customers, only: [:index,:show,:edit,:update]
-  end
-
-  scope module: :public do
-   get "customers/unsubscribe"=>"customers#unsubscribe", as: 'unsubscribe'
-   patch "customers/withdraw"=>"customers#withdraw", as: 'withdraw'
-   resources :customers, only: [:show,:update]
-   resources :addresses, only: [:index, :create, :destroy, :edit, :update]
-   resources :cart_item, only: [:index, :show, :create, :update, :destroy]
-  end
-  
-  resources :cart_item, only: [:index, :show, :create, :update, :destroy]
+    #devise
+    devise_for :customers,skip:[:passwords], controllers: {registrations: "public/registrations",sessions: 'public/sessions'}
+    devise_for :admin,skip:[:registrations, :passwords], controllers: {sessions: "admin/sessions"}
+    # public
+    namespace :public do
+     resources :items, only:[:index, :show]
+     resources :orders, only:[:new, :index, :show, :create]
+     post "orders/log"=>"orders#log"
+     get "orders/thanks"=>"oeders#thanks"
+    end
+    #admin
+    namespace :admin do
+     resources :genres
+     resources :items, only: [:index, :show, :edit, :update, :create, :new]
+     resources :customers, only: [:index,:show,:edit,:update]
+    end
+    #public
+    scope module: :public do
+      get "customers/unsubscribe"=>"customers#unsubscribe", as: 'unsubscribe'
+      patch "customers/withdraw"=>"customers#withdraw", as: 'withdraw'
+      delete "cart_items/all_destroy" => "cart_items#all_destroy", as: "all_destroy"
+      resources :customers, only: [:show,:update]
+      resources :addresses, only: [:index, :create, :destroy, :edit, :update]
+      resources :cart_items, only: [:index, :show, :create, :update, :destroy]
+    end
 
 end
