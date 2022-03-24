9c33d316af9813367efe80b2a80261116211b0cb
EC2 Default User <ec2-user@ip-172-31-17-194.ec2.internal>
2022-03-24 08:01:59 +0000

    migrationファイル（小島）の修正

--- db/migrate/20220317014758_create_items.rb
+++ db/migrate/20220317014758_create_items.rb
@@ -7,12 +7,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
       t.boolean :is_active, null: false, default: "販売中"
       t.integer :price, null: false
 
-<<<<<<< HEAD
 
-
-
-=======
->>>>>>> 4c05a14b004035c727df7e1d37946148bbe2e00d
       t.timestamps
     end
   end
