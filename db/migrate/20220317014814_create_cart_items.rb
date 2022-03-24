a6f9f751199260d35bdb0d1833defabb8a5b33bf
taku472 <taku.3303@icloud.com>
2022-03-24 01:54:28 +0000

    test

--- db/migrate/20220317014814_create_cart_items.rb
+++ db/migrate/20220317014814_create_cart_items.rb
@@ -2,7 +2,8 @@ class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
       t.integer :count, null: false
-
+      t.integer :customer_id
+      t.integer :item_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_19_091607) do
+ActiveRecord::Schema.define(version: 2022_03_20_051524) do
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.integer "record_id", null: false
+    t.integer "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.integer "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "addresses", force: :cascade do |t|
     t.string "customer_id"
@@ -35,6 +63,8 @@ ActiveRecord::Schema.define(version: 2022_03_19_091607) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "count", null: false
+    t.integer "customer_id"
+    t.integer "item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -72,6 +102,7 @@ ActiveRecord::Schema.define(version: 2022_03_19_091607) do
     t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "genre_id"
   end
 
   create_table "order_items", force: :cascade do |t|
@@ -84,9 +115,9 @@ ActiveRecord::Schema.define(version: 2022_03_19_091607) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "shipping_fee", default: 800, null: false
-    t.integer "payment_method"
+    t.integer "payment_method", default: 0
     t.integer "billing_amount"
-    t.integer "status"
+    t.integer "status", default: 0
     t.string "address"
     t.string "postal_code"
     t.string "receiver_name"
@@ -106,4 +137,6 @@ ActiveRecord::Schema.define(version: 2022_03_19_091607) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
