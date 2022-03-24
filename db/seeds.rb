ec157109342013781b69a28513e4a59b30262e7e
EC2 Default User <ec2-user@ip-172-31-17-32.ec2.internal>
2022-03-24 07:47:39 +0000

    address修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-<<<<<<< HEAD
 Admin.create!(email: 'a@a', password: 'aaaaaa')
-=======
-
- Admin.create!(email: 'a@a', password: 'aaaaaa')
->>>>>>> origin/develop
