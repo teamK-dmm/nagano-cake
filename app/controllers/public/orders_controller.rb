6a9b266f97bc408b6572131a2393c1f8a664dc32
EC2 Default User <ec2-user@ip-172-31-41-214.ec2.internal>
2022-03-26 04:40:45 +0000

    注文機能登録済み住所から選択修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,12 +11,12 @@ class Public::OrdersController < ApplicationController
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
     elsif params[:order][:address_option] == "1"
-      @address = Address.find_by(id: params[:order][:address])
-      @order.receiver_name = @address.name
-      @order.postal_code = @address.posal_code
+      @address = Address.find(params[:order][:address_id])
+      @order.receiver_name = @address.receiver_name
+      @order.postal_code = @address.postal_code
       @order.address = @address.address
     elsif params[:order][:address_option] == "2"
-      @order.receiver_name = params[:order][:name]
+      @order.receiver_name = params[:order][:receiver_name]
       @order.postal_code = params[:order][:postal_code]
       @order.address = params[:order][:address]
     end
